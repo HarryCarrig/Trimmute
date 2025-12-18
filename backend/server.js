0dadf4853e0a46c8bb3cb2cbb3fb64ae4b41058c
Harry Carrig <harrycarrig05@icloud.com>
2025-12-18 13:44:30 +0000

    Use Render PORT env var

--- backend/server.js
+++ backend/server.js
@@ -219,7 +219,7 @@ app.delete("/bookings/:id", async (req, res) => {
 
 // ---------- START SERVER ----------
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
